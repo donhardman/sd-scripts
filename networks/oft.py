c9bca6e3c0b6f1d92f4006ed71fd184361e7c33d
Don Hardman <don.hardman@muvon.io>
2023-12-22 15:45:10 +0700

    Fix oft arguments

--- networks/oft.py
+++ networks/oft.py
@@ -329,7 +329,7 @@ class OFTNetwork(torch.nn.Module):
         print(f"weights are merged")
 
     # 二つのText Encoderに別々の学習率を設定できるようにするといいかも
-    def prepare_optimizer_params(self, text_encoder_lr, unet_lr, default_lr):
+    def prepare_optimizer_params(self, text_encoder_lr, unet_lr):
         self.requires_grad_(True)
         all_params = []
 
